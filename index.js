b6bfb2cfd17ebeee113ee7e6e9c274b7e8aeb1bf
chandu-kurva <saee1604@gmail.com>
2021-04-02 14:03:10 +0000

    my-new-node-app

---
+++ index.js
@@ -0,0 +1,11 @@
+var addDays = require("date-fns/addDays");
+
+const result = addDays(new Date(2022, 7, 5), 5);
+
+console.log(result);
+
+var path = require("path");
+
+let filePath = path.join("karnataka", "raichur", "karridana");
+
+console.log(filePath);
